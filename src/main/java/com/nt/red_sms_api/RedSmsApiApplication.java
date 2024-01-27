adf6374f855f22498aff28517a529611d96b98a7
คนที่คุณก็รู้ว่าใคร <pawat@computerlogy.com>
2024-01-27 16:51:14 +0700

    init red_sms_api

---
+++ src/main/java/com/nt/red_sms_api/RedSmsApiApplication.java
@@ -0,0 +1,13 @@
+package com.nt.red_sms_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedSmsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedSmsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nt/red_sms_api/RedSmsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nt.red_sms_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedSmsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
