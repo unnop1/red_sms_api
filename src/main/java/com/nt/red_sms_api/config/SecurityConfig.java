3fd3a6a1d763010911026d25ac73f4faa1a9c706
คนที่คุณก็รู้ว่าใคร <pawat@computerlogy.com>
2024-04-07 23:39:06 +0700

    update deploy

--- src/main/java/com/nt/red_sms_api/config/SecurityConfig.java
+++ src/main/java/com/nt/red_sms_api/config/SecurityConfig.java
@@ -5,18 +5,24 @@ import com.nt.red_sms_api.Auth.JwtAuthenticationFilter;
 import com.nt.red_sms_api.Auth.UsernamePasswordAuthenticationFilter;
 
 import javax.servlet.FilterChain;
-import javax.servlet.annotation.HttpConstraint;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.ReactiveAuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.ServerAuthenticationEntryPoint;
+import org.springframework.security.web.server.context.ServerSecurityContextRepository;
+import org.springframework.web.server.WebFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -30,7 +36,7 @@ public class SecurityConfig {
     @Autowired
     private PasswordEncoder passwordEncoder;
     @Bean
-    public FilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         //configuration
         http.csrf(csrf -> csrf.disable())
                 .cors(cors -> cors.disable())
@@ -43,6 +49,14 @@ public class SecurityConfig {
                 .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)); // nothing to save on server
         http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
+    }
+    private ServerSecurityContextRepository securityContextRepository() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'securityContextRepository'");
+    }
+    private ReactiveAuthenticationManager authenticationManager() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'authenticationManager'");
     }
     @Bean
     public DaoAuthenticationProvider doDaoAuthenticationProvider() {
