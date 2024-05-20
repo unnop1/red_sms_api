3a18945e5df9b7299aa8860fcc4c14f054deaba8
คนที่คุณก็รู้ว่าใคร <pawat@computerlogy.com>
2024-05-20 15:05:21 +0700

    deploy jboss nt success

--- src/main/java/com/nt/red_sms_api/RedSmsApiApplication.java
+++ src/main/java/com/nt/red_sms_api/RedSmsApiApplication.java
@@ -5,16 +5,27 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
-public class RedSmsApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RedSmsApiApplication.class, args);
-	}
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+
+@SpringBootApplication
+public class RedSmsApiApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedSmsApiApplication.class, args);
+    }
+
+    //If you need a traditional war deployment we need to extend the SpringBootServletInitializer
+    //This helps us deploy war files to Jboss containers
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder springApplicationBuilder) {
+        return springApplicationBuilder.sources(RedSmsApiApplication.class);
+    }
 
 	@Bean
 	public ModelMapper modelMapper(){
 		return new ModelMapper();
 	}
-
 }
