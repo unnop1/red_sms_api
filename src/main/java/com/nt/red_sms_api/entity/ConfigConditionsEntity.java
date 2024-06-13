72ce2231e2cfdfa8e931915fec400853be295f40
คนที่คุณก็รู้ว่าใคร <pawat@computerlogy.com>
2024-06-13 10:33:33 +0700

    test deploy

--- src/main/java/com/nt/red_sms_api/entity/ConfigConditionsEntity.java
+++ src/main/java/com/nt/red_sms_api/entity/ConfigConditionsEntity.java
@@ -23,55 +23,55 @@ public class ConfigConditionsEntity {
         @Id
         @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "config_conditions_seq")
         @SequenceGenerator(name = "config_conditions_seq", allocationSize = 1)
-        @Column(name = "conditions_ID")
+        @Column(name = "CONDITIONS_ID")
         private Long conditionsID;
 
-        @Column(name = "order_type_MainID", unique = false,nullable = true)
+        @Column(name = "ORDER_TYPE_MAINID", unique = false,nullable = true)
         private Long order_type_MainID = null;
         
-        @Column(name = "orderType", unique = false,nullable = true)
+        @Column(name = "ORDERTYPE", unique = false,nullable = true)
         private String orderType = null;
 
-        @Column(name = "refID", unique = false,nullable = true)
+        @Column(name = "REDID", unique = false,nullable = true)
         private String refID = null;
 
-        @Column(name = "date_Start", unique = false,nullable = true)
+        @Column(name = "DATE_START", unique = false,nullable = true)
         private Timestamp date_Start = null;
 
-        @Column(name = "date_End", unique = false,nullable = true)
+        @Column(name = "DATE_END", unique = false,nullable = true)
         private Timestamp date_End = null;
 
-        @Column(name = "message", unique = false,nullable = true)
+        @Column(name = "MESSAGE", unique = false,nullable = true)
         private String message = null;
 
-        @Column(name = "conditions_or", unique = false,nullable = true)
+        @Column(name = "CONDITIONS_OR", unique = false,nullable = true)
         private Clob conditions_or = null;
 
-        @Column(name = "conditions_and", unique = false,nullable = true)
+        @Column(name = "CONDITIONS_AND", unique = false,nullable = true)
         private Clob conditions_and = null;
 
         @Column(name = "created_Date", unique = false,nullable = true)
         private Timestamp created_Date = null;
 
-        @Column(name = "created_By", unique = false,nullable = true)
+        @Column(name = "CREATED_BY", unique = false,nullable = true)
         private String created_By = null;
 
-        @Column(name = "updated_Date", unique = false,nullable = true)
+        @Column(name = "UPDATED_DATE", unique = false,nullable = true)
         private Timestamp updated_Date = null;
 
-        @Column(name = "updated_By", unique = false,nullable = true)
+        @Column(name = "UPDATED_BY", unique = false,nullable = true)
         private String updated_By = null;
 
-        @Column(name = "is_delete", unique = false,nullable = true)
+        @Column(name = "ID_DELETE", unique = false,nullable = true)
         private Integer is_delete = 0;
 
-        @Column(name = "is_enable", unique = false,nullable = true)
+        @Column(name = "IS_ENABLE", unique = false,nullable = true)
         private Integer is_enable = 1;
 
-        @Column(name = "is_Delete_By", unique = false,nullable = true)
+        @Column(name = "IS_DELETE_BY", unique = false,nullable = true)
         private String is_Delete_By = null;
 
-        @Column(name = "is_Delete_Date", unique = false,nullable = true)
+        @Column(name = "IS_DELETE_DATE", unique = false,nullable = true)
         private Timestamp is_Delete_Date = null;
 
         @Column(name = "conditions_or_select", unique = false,nullable = true)
