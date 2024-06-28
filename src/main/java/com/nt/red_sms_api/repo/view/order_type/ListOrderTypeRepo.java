f8ac2132c738c645ea1b8964d4a2af83997b9dee
unnop1 <unnop.lapanalarploy@gmail.com>
2024-06-28 23:38:47 +0700

    Update Where counter unmatch

--- src/main/java/com/nt/red_sms_api/repo/view/order_type/ListOrderTypeRepo.java
+++ src/main/java/com/nt/red_sms_api/repo/view/order_type/ListOrderTypeRepo.java
@@ -1,5 +1,8 @@
 package com.nt.red_sms_api.repo.view.order_type;
 
+import java.sql.Timestamp;
+import java.util.List;
+
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -7,9 +10,6 @@ import org.springframework.data.repository.query.Param;
 
 import com.nt.red_sms_api.entity.OrderTypeEntity;
 import com.nt.red_sms_api.entity.view.order_type.ListOrderType;
-
-import java.sql.Timestamp;
-import java.util.List;
 
 public interface ListOrderTypeRepo extends JpaRepository<OrderTypeEntity,Long> {
     
@@ -27,7 +27,7 @@ public interface ListOrderTypeRepo extends JpaRepository<OrderTypeEntity,Long> {
                         (SELECT COUNT(sms_gw.GID)
                         FROM sms_gateway sms_gw
                         WHERE odt.MainID = sms_gw.order_type_mainID
-                        AND sms_gw.Is_Status = 2
+                        AND sms_gw.Is_Status = 2 OR sms_gw.Is_Status = 4 
                         AND (sms_gw.CREATED_DATE BETWEEN :start_time AND :end_time)) AS TotalUnSend
                   FROM order_type odt
                   WHERE odt.Is_Delete = 0
