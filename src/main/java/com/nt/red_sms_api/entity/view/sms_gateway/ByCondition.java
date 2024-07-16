7581033bf097c1c25ac691b34a5b761beac1f0e3
คนที่คุณก็รู้ว่าใคร <pawat@computerlogy.com>
2024-07-16 23:52:36 +0700

    update limit 0, add ordertype_main id

--- src/main/java/com/nt/red_sms_api/entity/view/sms_gateway/ByCondition.java
+++ src/main/java/com/nt/red_sms_api/entity/view/sms_gateway/ByCondition.java
@@ -4,7 +4,6 @@ package com.nt.red_sms_api.entity.view.sms_gateway;
 import java.sql.Timestamp;
 
 public interface ByCondition {
-        // String getDATE_ONLY();
         Long getCONDITIONS_ID();
         Integer getTOTALEVENT();
         Integer getTOTALUNMATCH();
