06284522692b26c6b81a0afe14e947c89b773e90
คนที่คุณก็รู้ว่าใคร <pawat@computerlogy.com>
2024-08-01 06:38:37 +0700

    update page sort

--- src/main/java/com/nt/red_sms_api/service/imp/SmsGatewayImp.java
+++ src/main/java/com/nt/red_sms_api/service/imp/SmsGatewayImp.java
@@ -205,7 +205,7 @@ public class SmsGatewayImp implements SmsGatewayService{
             if(offset.equals(0) && limit.equals(0)){
                 List<BySending> smsGatewayEntities = bySendingRepo.ListBySending(startTime, endTime);
                 Integer count = bySendingRepo.getListBySendingTotalCount(startTime, endTime);
-                ManualSort.sortBySendingDate(smsGatewayEntities, sortName, sortBy);
+                // ManualSort.sortBySendingDate(smsGatewayEntities, sortName, sortBy);
                 resp.setCount(count);
                 resp.setData(smsGatewayEntities);
                 return resp;
@@ -213,7 +213,7 @@ public class SmsGatewayImp implements SmsGatewayService{
 
             List<BySending> smsGatewayEntities = bySendingRepo.ListBySending(startTime, endTime, PageRequest.of(page, limit));
             Integer count = bySendingRepo.getListBySendingTotalCount(startTime, endTime);
-            ManualSort.sortBySendingDate(smsGatewayEntities, sortName, sortBy);
+            // ManualSort.sortBySendingDate(smsGatewayEntities, sortName, sortBy);
             resp.setCount(count);
             resp.setData(smsGatewayEntities);
             return resp;
